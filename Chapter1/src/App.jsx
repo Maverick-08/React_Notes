a8e4957ff3b7a5fe45e3806317b2c8b319ae9083
Vivek <ojhavivek24@gmail.com>
2024-06-14 12:05:55 +0530

    Chapter 1

--- Chapter1/src/App.jsx
+++ Chapter1/src/App.jsx
@@ -1,10 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function App() {
+  const [count,setCount] = useState(0);
+  let x = 1; // This will not cause rerender
+
+  function change(){
+    ++x;
+    console.log(x);
+  }
 
   return (
     <div>
+        <Component1  change={change} setCount={setCount} x={x} count={count}/>
+    </div>
+  )
+}
 
+function Component1({change,setCount,x,count}){
+  return(
+    <div>
+      <p>X : {x}</p>
+      <p>count : {count}</p>
+      <button onClick={change}>Change x</button><br />
+      <button onClick={()=>setCount(count+1)}>Change Count</button>
     </div>
   )
 }
